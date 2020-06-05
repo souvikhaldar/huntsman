b7bc326a61df6dfbfdf0d20276d6bd7532d3f79c
Souvik Haldar <souvik@Souviks-MacBook-Pro.local>
2020-06-05 15:49:23 +0530

    Start working on scanning IP for info

---
+++ cmd/ipMap.go
@@ -0,0 +1,72 @@
+package cmd
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
+
+	"github.com/spf13/cobra"
+)
+
+var ipCmd = &cobra.Command{
+	Use:   "iploc",
+	Short: "Fetch the location information of the IP",
+	Run: func(cmd *cobra.Command, args []string) {
+		var ip string
+		var err error
+		if len(args) == 0 {
+			fmt.Println("Provide the input")
+			return
+		}
+		if tcpDump {
+			ip, err = ParseIPFromTcpDump(args[0])
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+		} else {
+			ip = args[0]
+		}
+		resp, err := http.Get(fmt.Sprintf("http://ip-api.com/json/%s", ip))
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		defer resp.Body.Close()
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		fmt.Println("Body: ", string(body))
+		return
+
+	},
+}
+var tcpDump bool
+
+func init() {
+	rootCmd.AddCommand(ipCmd)
+	ipCmd.Flags().BoolVar(&tcpDump, "tcpdump", false, "Is the source from tcpdump")
+}
+
+func ParseIPFromTcpDump(tcpDump string) (string, error) {
+	split := strings.Split(tcpDump, "\n")
+	if len(split) == 0 {
+		return "", fmt.Errorf("Error in parsing tcpdump output: %s", "split")
+	}
+	newSplit := strings.SplitAfter(split[0], ">")[0]
+	sliceSplit := strings.Fields(newSplit)
+	if len(sliceSplit) < 3 {
+		return "", fmt.Errorf("Error in parsing tcpdump output: %s", "sliceSplit")
+	}
+	lastDot := strings.LastIndex(sliceSplit[2], ".")
+	if lastDot == -1 {
+		return "", fmt.Errorf("Error in parsing tcpdump output: %s", "lastDot")
+	}
+	if len(sliceSplit[2]) < lastDot {
+		return "", fmt.Errorf("Error in parsing tcpdump output: %s", "lastDot length")
+	}
+	return sliceSplit[2][:lastDot], nil
+}
